31112e6453328fb98a81868722b4a4b53b0614ca
Tau0x <zinoux@gmail.com>
2025-06-25 11:50:56 +0300

    admin

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Booking, Menu
 
 admin.site.register(Booking)
 admin.site.register(Menu)
-=======
-
-# Register your models here.
->>>>>>> 49ddca9892d8ce8080d3c1e09a4d79bda59804d4
