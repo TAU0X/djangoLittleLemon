153a57695f12ba0dfe56de65e7bbfd1cde60c65c
Tau0x <zinoux@gmail.com>
2025-06-25 10:33:58 +0300

    git ignore file

--- restaurant/models.py
+++ restaurant/models.py
@@ -13,3 +13,11 @@ class Booking(models.Model):
 
 
 # Add code to create Menu model
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True)
+
+    def __str__(self):
+        return self.name
